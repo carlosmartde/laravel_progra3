f3f2610422afc6a16e6e3283ccf90c1d1890d2bb
carlosmartde <160431626+carlosmartde@users.noreply.github.com>
2025-04-02 21:27:22 -0600

    mejoras en la vista de camiones

--- resources/views/Camiones/index.blade.php
+++ resources/views/Camiones/index.blade.php
@@ -13,25 +13,29 @@
             <table class="table table-striped table-hover align-middle">
                 <thead class="table-dark">
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Placa</th>
-                    <th scope="col">Modelo</th>
-                    <th scope="col">Marca</th>
-                    <th scope="col">Transporte</th>
-                    <th scope="col">Capacidad (toneladas)</th>
-                    <th scope="col">Opciones</th>
+                    <th scope="col" class="text-center">ID</th>
+                    <th scope="col" class="text-center">Placa</th>
+                    <th scope="col" class="text-center">Modelo</th>
+                    <th scope="col" class="text-center">Color</th>
+                    <th scope="col" class="text-center">Codigo interno</th>
+                    <th scope="col" class="text-center">Marca</th>
+                    <th scope="col" class="text-center">Transporte</th>
+                    <th scope="col" class="text-center">Capacidad (toneladas)</th>
+                    <th scope="col" class="text-center">Opciones</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach ($camiones as $camion)
                     <tr>
-                        <td>{{ $camion->id_camion }}</td>
-                        <td>{{ $camion->placa }}</td>
-                        <td>{{ $camion->modelo }}</td>
-                        <td>{{ $camion->marca->descripcion }}</td>
-                        <td>{{ $camion->transporte->nombre }}</td>
-                        <td>{{ $camion->capacidad_toneladas }}</td>
-                        <td>
+                        <td class="text-center">{{ $camion->id_camion }}</td>
+                        <td class="text-center">{{ $camion->placa }}</td>
+                        <td class="text-center">{{ $camion->modelo }}</td>
+                        <td class="text-center">{{ $camion->color }}</td>
+                        <td class="text-center">{{ $camion->codig_interno }}</td>
+                        <td class="text-center">{{ $camion->marca->descripcion }}</td>
+                        <td class="text-center">{{ $camion->transporte->nombre }}</td>
+                        <td class="text-center">{{ $camion->capacidad_toneladas }}</td>
+                        <td class="text-center">
                             <a href="{{ route('camiones.edit', $camion->id_camion) }}" class="btn btn-sm btn-warning me-2">
                                 <i class="fas fa-edit"></i> Editar
                             </a>
@@ -42,27 +46,6 @@
                                     <i class="fas fa-trash"></i> Eliminar
                                 </button>
                             </form>
-                            
-                            <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                            <script>
-                                function confirmDelete(camionId) {
-                                    Swal.fire({
-                                        title: "¿Estás seguro?",
-                                        text: "Esta acción no se puede deshacer.",
-                                        icon: "warning",
-                                        showCancelButton: true,
-                                        confirmButtonColor: "#d33",
-                                        cancelButtonColor: "#3085d6",
-                                        confirmButtonText: "Sí, eliminar",
-                                        cancelButtonText: "Cancelar"
-                                    }).then((result) => {
-                                        if (result.isConfirmed) {
-                                            document.getElementById('delete-form-' + camionId).submit();
-                                        }
-                                    });
-                                }
-                            </script>
-                            
                         </td>
                     </tr>
                 @endforeach
@@ -73,4 +56,24 @@
             {{ $camiones->links('pagination::bootstrap-5') }}
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        function confirmDelete(camionId) {
+            Swal.fire({
+                title: "¿Estás seguro?",
+                text: "Esta acción no se puede deshacer.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                confirmButtonText: "Sí, eliminar",
+                cancelButtonText: "Cancelar"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('delete-form-' + camionId).submit();
+                }
+            });
+        }
+    </script>
 @endsection
